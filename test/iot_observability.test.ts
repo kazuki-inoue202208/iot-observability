662571093723f4da3fda7b8d3ec5171599f3f87e
kazuki-inoue202208 <kazuki.inoue@ncdc.co.jp>
2026-05-31 14:54:11 +0900

    Initial commit

---
+++ test/iot_observability.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IotObservability from '../lib/iot_observability-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iot_observability-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IotObservability.IotObservabilityStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
